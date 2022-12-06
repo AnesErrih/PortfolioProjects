f55f446524497b3815d6d4458d18901c076de18c
AnesErrih <119816807+AnesErrih@users.noreply.github.com>
2022-12-06 22:07:38 +0100

    Rename Covid.sql to Project1_Covid.sql

--- Project1_Covid.sql
+++ Project1_Covid.sql
@@ -11,7 +11,7 @@ GO
 
 
 --		Total Cases vs Total Deaths
---   la chance de mourir si tu es touché par le covid dans ton pays  : 
+--   la chance de mourir si tu es touchÃ© par le covid dans ton pays  : 
 select [location],[date],[total_cases],[total_deaths] , ([total_deaths]/[total_cases])*100 as chance_de_mourir
 from [dbo].[covid_death]
 where continent is not null and population  is not null
@@ -19,22 +19,22 @@ group by [continent],[location],[date],[population],[total_cases],[total_deaths]
 order by 1,2
 
 --  population vs total cases
---pourcentage de population touché par le covid  :
+--pourcentage de population touchÃ© par le covid  :
 
-select [location],[date],[population],[total_cases], ([total_cases]/[population])*100 as pourcentage_touché
+select [location],[date],[population],[total_cases], ([total_cases]/[population])*100 as pourcentage_touchÃ©
 from [dbo].[covid_death] 
 where  [location] like '%france%' 
 and  continent is not null and population  is not null
-order by pourcentage_touché desc
+order by pourcentage_touchÃ© desc
 
--- les pays les plus touché par le covid par rapport au population 
-select [location],[population] , max([total_cases]) as total_cases, max(([total_cases]/[population]))*100 as pourcentage_touché
+-- les pays les plus touchÃ© par le covid par rapport au population 
+select [location],[population] , max([total_cases]) as total_cases, max(([total_cases]/[population]))*100 as pourcentage_touchÃ©
 from [dbo].[covid_death]
 where continent is not null and population  is not null
 group by [location],[population]
 order by [location]
 
--- les pays avec les plus cas de décès : 
+-- les pays avec les plus cas de dÃ©cÃ¨s : 
 select [location] , max([total_deaths]) as total_mort
 from [dbo].[covid_death]
 where continent is not null and population  is not null
@@ -43,7 +43,7 @@ order by total_mort desc
 
 -- continents : 
 
---les continents avec les plus cas de décès :
+--les continents avec les plus cas de dÃ©cÃ¨s :
 select [continent] , max([total_deaths]) as total_mort
 from [dbo].[covid_death]
 where [continent] is not null and [population] is not null 
@@ -56,11 +56,11 @@ where [continent] is not null and [population] is not null
 
 -- vaccination : 
 -- transformer 
--- pourcentage de population vacciné :
+-- pourcentage de population vaccinÃ© :
 select [dbo].[covid_death].[location],[dbo].[covid_death].[date],[dbo].[covid_death].[population],
 [dbo].[covid_vacc].[new_vaccinations], 
 sum([dbo].[covid_vacc].[new_vaccinations]) over (partition by [dbo].[covid_death].[location] order by [dbo].[covid_death].[location],
-	[dbo].[covid_death].[date]) as le_mise_à_jour_de_total_vac
+	[dbo].[covid_death].[date]) as le_mise_Ã _jour_de_total_vac
 from [dbo].[covid_death]
 join [dbo].[covid_vacc]
 	on [dbo].[covid_death].location=[dbo].[covid_vacc].location
@@ -70,13 +70,13 @@ order by 1,2
 
 
 ---Cte
-with popultaionVSvacc ([location],[date],[population],[new_vaccinations],le_mise_à_jour_de_total_vac)
+with popultaionVSvacc ([location],[date],[population],[new_vaccinations],le_mise_Ã _jour_de_total_vac)
 as
 (
 select [dbo].[covid_death].[location],[dbo].[covid_death].[date],[dbo].[covid_death].[population],
 [dbo].[covid_vacc].[new_vaccinations], 
 sum([dbo].[covid_vacc].[new_vaccinations]) over (partition by [dbo].[covid_death].[location] order by [dbo].[covid_death].[location],
-	[dbo].[covid_death].[date]) as le_mise_à_jour_de_total_vac
+	[dbo].[covid_death].[date]) as le_mise_Ã _jour_de_total_vac
 from [dbo].[covid_death]
 join [dbo].[covid_vacc]
 	on [dbo].[covid_death].location=[dbo].[covid_vacc].location
@@ -84,7 +84,7 @@ join [dbo].[covid_vacc]
 where [dbo].[covid_death].continent is not null and [dbo].[covid_death].[population] is not null
 --order by 1,2
 )
-select *,(le_mise_à_jour_de_total_vac/[population])*100 as pourcentage_vacciné
+select *,(le_mise_Ã _jour_de_total_vac/[population])*100 as pourcentage_vaccinÃ©
 from popultaionVSvacc
 where location like '%canada%'
 order by 2
@@ -99,14 +99,14 @@ location nvarchar(255),
 date datetime,
 population float,
 new_vaccinations float,
-le_mise_à_jour_de_total_vac float
+le_mise_Ã _jour_de_total_vac float
 );
 
 insert into #avancement_de_vaccination
 select [dbo].[covid_death].[continent],[dbo].[covid_death].[location],[dbo].[covid_death].[date],[dbo].[covid_death].[population],
 [dbo].[covid_vacc].[new_vaccinations], 
 sum([dbo].[covid_vacc].[new_vaccinations]) over (partition by [dbo].[covid_death].[location] order by [dbo].[covid_death].[location],
-	[dbo].[covid_death].[date]) as le_mise_à_jour_de_total_vac
+	[dbo].[covid_death].[date]) as le_mise_Ã _jour_de_total_vac
 from [dbo].[covid_death]
 join [dbo].[covid_vacc]
 	on [dbo].[covid_death].location=[dbo].[covid_vacc].location
@@ -114,17 +114,17 @@ join [dbo].[covid_vacc]
 where [dbo].[covid_death].continent is not null and [dbo].[covid_death].[population] is not null
 --order by 1,2
 
-select * ,(le_mise_à_jour_de_total_vac/[population])*100 as pourcentage_vacciné
+select * ,(le_mise_Ã _jour_de_total_vac/[population])*100 as pourcentage_vaccinÃ©
 from #avancement_de_vaccination
 order by 2,3
 
 
--- créer des view : 
+-- crÃ©er des view : 
 create view avancement_de_vaccination as 
 select [dbo].[covid_death].[continent],[dbo].[covid_death].[location],[dbo].[covid_death].[date],[dbo].[covid_death].[population],
 [dbo].[covid_vacc].[new_vaccinations], 
 sum([dbo].[covid_vacc].[new_vaccinations]) over (partition by [dbo].[covid_death].[location] order by [dbo].[covid_death].[location],
-	[dbo].[covid_death].[date]) as le_mise_à_jour_de_total_vac
+	[dbo].[covid_death].[date]) as le_mise_Ã _jour_de_total_vac
 from [dbo].[covid_death]
 join [dbo].[covid_vacc]
 	on [dbo].[covid_death].location=[dbo].[covid_vacc].location
